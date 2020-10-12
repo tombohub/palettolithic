254909157c5b65d3002bc53115e5a0a3e8ae0a4c
babaroga <mama@mia.com>
2020-10-12 01:22:38 -0400

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Form from "./Form";
 import Palette from "./Palette";
+import Menu from "./Menu";
 
 /**
  * Main component that displays the first page with form and palette
@@ -11,7 +12,7 @@ function Main(props) {
   return (
     <div className="text-gray-900 bg-gray-100 p-4">
       <div className="container mx-auto">
-        <Header />
+        <Menu />
 
         {/* passing onSubmit from App to Form */}
         <Form onSubmit={props.onSubmit} />
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function Menu(props) {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Menu;
