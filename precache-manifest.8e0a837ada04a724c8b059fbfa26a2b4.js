686b2b21a3d314541bb1ad705a6a737223f0e55c
babaroga <mama@mia.com>
2020-10-15 15:59:15 -0400

    Updates

---
+++ precache-manifest.8e0a837ada04a724c8b059fbfa26a2b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227535a0a957e28eb8cd5e5f36f3d697",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd0f3a45e675c50569d9",
+    "url": "/static/css/2.63d8dba5.chunk.css"
+  },
+  {
+    "revision": "3ea03dd021c52d2c0144",
+    "url": "/static/css/main.348d9bf0.chunk.css"
+  },
+  {
+    "revision": "dd0f3a45e675c50569d9",
+    "url": "/static/js/2.38cdf493.chunk.js"
+  },
+  {
+    "revision": "2a2ad68459b1a6f423c112665d485134",
+    "url": "/static/js/2.38cdf493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea03dd021c52d2c0144",
+    "url": "/static/js/main.f108dfa4.chunk.js"
+  },
+  {
+    "revision": "ffbb58df1c6b0924fcae",
+    "url": "/static/js/runtime-main.1465be04.js"
+  },
+  {
+    "revision": "397ed4af808cce25f33f45918780ed3d",
+    "url": "/static/media/frijole-all-400-normal.397ed4af.woff"
+  },
+  {
+    "revision": "1dfeeed6ee9dcfd805af87741b2e4817",
+    "url": "/static/media/frijole-latin-400-normal.1dfeeed6.woff2"
+  },
+  {
+    "revision": "9c442ce54c2b62471178a62f0b2e30a1",
+    "url": "/static/media/schoolbell-all-400-normal.9c442ce5.woff"
+  },
+  {
+    "revision": "1c69d18205d2b024adb296bbae9eb9f4",
+    "url": "/static/media/schoolbell-latin-400-normal.1c69d182.woff2"
+  }
+]);
