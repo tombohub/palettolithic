5b6a4cf3145aeda8b0a7bf23301d4e5d9921cd67
babaroga <mama@mia.com>
2020-10-15 00:07:29 -0400

    copied status fixed

--- src/components/CodeContent.jsx
+++ src/components/CodeContent.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 // scripts
 import generateTailwind from "../scripts/generateTailwind";
@@ -17,6 +19,29 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
  * @param {*} props
  */
 function CodeContent(props) {
+  //
+  /**
+   * WHAT: copy status to display if code is copied.
+   * WHY: so the user gets feedback if code is copied to clipboard.
+   * It uses useEffect to change back to 'Copy' anytime the palette or active framework changes.
+   * When user clicks on "Copy" span button then the handleOnClick sets the state to "Copied"
+   */
+  const [copyStatus, setCopyStatus] = useState("Copy");
+
+  useEffect(() => {
+    setCopyStatus("Copy");
+  }, [props.palette, props.activeFramework]);
+
+  function handleOnClick(e) {
+    setCopyStatus("Copied");
+  }
+
+  /* ------------------------------ Code handle ----------------------------- */
+
+  /**
+   * This part of code applies syntax highlighting and copy button
+   */
+
   SyntaxHighlighter.registerLanguage("javascript", js);
   SyntaxHighlighter.registerLanguage("scss", scss);
 
@@ -24,8 +49,11 @@ function CodeContent(props) {
     <>
       <CopyToClipboard text={generateTailwind(props.palette)}>
         <div className="flex justify-end">
-          <span className="cursor-pointer font-mono bg-teal-300 rounded text-teal-900 px-1 hover:bg-teal-900 hover:text-teal-300 transition duration-100">
-            Copy
+          <span
+            onClick={handleOnClick}
+            className="cursor-pointer font-mono bg-teal-300 rounded text-teal-900 px-1 hover:bg-teal-900 hover:text-teal-300 transition duration-100"
+          >
+            {copyStatus}
           </span>
         </div>
       </CopyToClipboard>
@@ -39,8 +67,11 @@ function CodeContent(props) {
     <>
       <CopyToClipboard text={generateBootstrap(props.palette)}>
         <div className="flex justify-end">
-          <span className="cursor-pointer font-mono bg-purple-300 rounded text-purple-900 px-1 hover:bg-purple-900 hover:text-purple-300 transition duration-100">
-            Copy
+          <span
+            onClick={handleOnClick}
+            className="cursor-pointer font-mono bg-purple-300 rounded text-purple-900 px-1 hover:bg-purple-900 hover:text-purple-300 transition duration-100"
+          >
+            {copyStatus}
           </span>
         </div>
       </CopyToClipboard>
@@ -49,6 +80,8 @@ function CodeContent(props) {
       </SyntaxHighlighter>
     </>
   );
+
+  /* -------------------------------- Render -------------------------------- */
 
   function renderCode(activeFramework) {
     switch (activeFramework) {
